110cf422141e913242d2072d3586db7754c799a8
Alexander Reitzel <funtimecoding@gmail.com>
2015-06-12 19:56:09 +0200

    unify java example projects

--- src/main/java/org/funtimecoding/log4j/example/ExampleMain.java
+++ src/main/java/org/funtimecoding/log4j/example/ExampleMain.java
@@ -3,15 +3,10 @@ package org.funtimecoding.log4j.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/**
- * @author shiin
- */
 public class ExampleMain {
-
     private static final Logger logger = LogManager.getLogger(ExampleMain.class);
 
     public static void main(String[] args) {
-
         logger.error("Hello World");
     }
 }
